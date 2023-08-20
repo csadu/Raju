2afab4f45dc18127159d282bdb592a9df765734d
saduchanti <lchantisadu@gmail.com>
2023-08-20 21:15:50 +0530

    second

---
+++ src/main/java/com/example/Raju/RajuApplication.java
@@ -0,0 +1,13 @@
+package com.example.Raju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Raju/RajuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Raju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
